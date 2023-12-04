7aceb59eb824a4d93461dcb812c2e99049118a66
stano <freespirit@users.noreply.github.com>
2023-12-04 21:31:35 +0200

    Solve day 1

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use clap::Parser;
+
+use aoc2023::day_01::{solve_part_1, solve_part_2};
+
+/// Solve a daily puzzle from Advent of Code 2023.
+#[derive(Parser, Debug)]
+struct Args {
+    /// File with input text for the day.
+    input_file: String,
+}
+
+fn main() {
+    let args = Args::parse();
+    println!("{}", solve_part_2(args.input_file.as_str()));
+}
